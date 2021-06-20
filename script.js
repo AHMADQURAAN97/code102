154d71fe84ee7f9373902e3ae948abaf861790e8
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-06-20 19:38:47 +0300

    build proj contain alot of js skills

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Welcome To WebPage For University"); 
+
+let username=prompt("Enter Your ID number :");
+
+
+while(username>100) {
+    alert("username is wrong");
+    username=prompt ("re-type your username correct");
+};
+
+
+ 
+
+alert("Welcome our students");
