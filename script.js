bc0da6fd4be7a530496316a15915a2eff0032e40
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-06-25 18:54:02 +0300

    add prompts

--- script.js
+++ script.js
@@ -2,13 +2,27 @@ alert("Welcome To WebPage For University");
 
 let username=prompt("Enter Your ID number :");
 
-
-while(username>100) {
+if(username>100){
     alert("username is wrong");
     username=prompt ("re-type your username correct");
+}; 
+if(username<100){
+    alert("correct Answre")
 };
 
 
+
+let username1=prompt("You are student or visitor?")
+if(username1=="visitor"){
+    alert("We wish you a nice visit to our site");
+}else {
+   alert("Hello our beautiful student")
+};
  
 
+let username2=prompt("What is your university major?")
+alert ("Excellent choice");
+
+let username3=prompt("Enter Your Name")
+alert ("Beautiful Name" + " " + username3)
 alert("Welcome our students");
